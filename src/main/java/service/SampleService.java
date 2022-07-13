df44e2dbc1ec9d38533146cef9324e69260ef407
jaishik <jaishik673@gmail.com>
2022-07-13 02:22:23 -0400

    branch

--- src/main/java/service/SampleService.java
+++ src/main/java/service/SampleService.java
@@ -4,6 +4,6 @@ public class SampleService {
 
 
     public String s = "here is your sample, rahul";
-    public String a = "here is your sample, mithali";
+    public String a = "here is your sample, mike";
 }
 
