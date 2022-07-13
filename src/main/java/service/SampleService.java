62c2fb43315a05a15acd1558830638ab6d055bb0
jaishik <jaishik673@gmail.com>
2022-07-13 02:28:31 -0400

    m

--- src/main/java/service/SampleService.java
+++ src/main/java/service/SampleService.java
@@ -4,6 +4,6 @@ public class SampleService {
 
 
     public String s = "here is your sample, rahul";
-    public String a = "here is your sample, mersel";
+    public String a = "here is your sample, goat";
 }
 
