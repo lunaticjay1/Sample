dd5b477133c0f89359eaa6cdbead719a804ea384
lunaticjay1 <46759393+lunaticjay1@users.noreply.github.com>
2022-07-13 02:26:34 -0400

    Update SampleService.java

--- src/main/java/service/SampleService.java
+++ src/main/java/service/SampleService.java
@@ -5,7 +5,7 @@ public class SampleService {
 
     public String s = "here is your sample, rahul";
 
-    public String a = "here is your sample, mersel";
+    public String a = "here is your sample, mervel";
 
 }
 
